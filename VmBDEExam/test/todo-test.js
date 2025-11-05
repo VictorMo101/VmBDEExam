312818a2590ab9dc99ebb82c9dd0362d934dffaf
SiegVive <vicmor01@easv365.dk>
2025-11-05 02:40:54 +0100

    feat: Workflow running testcafe test on todo

---
+++ VmBDEExam/test/todo-test.js
@@ -0,0 +1,12 @@
+import { Selector } from "testcafe";
+
+fixture("Todo Test One")
+    .page("https://verycookedhotel.dk/test/todo");
+
+test("Adding todo", async t => {
+    await t
+        .typeText(Selector("#todo-input"), "Now i click instead of enter")
+        .click(Selector("#addTodoButton"))
+        .typeText(Selector("#todo-input"), "Now i enter")
+        .pressKey("enter")
+});
